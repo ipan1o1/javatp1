4f002cd8f0481de5fd39880dcc0e46e72a26d6ec
Ipan <65251445+ipan1o1@users.noreply.github.com>
2025-09-09 17:43:06 +0200

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class fibonacci {
+
+    public static int fibo1(int n){
+        int result = 0;
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Choisissez un nombre: ");
+        int n = sc.nextInt();
+        fibo1(n);
+    }
+}
